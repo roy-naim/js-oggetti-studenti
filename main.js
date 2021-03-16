8dbfbac874c4d3821f3c153f3cde2d1f2c19b63a
Roy <b.roynaim@gmail.com>
2021-03-16 15:03:29 +0100

    Oggetto con studente dettaglio

---
+++ main.js
@@ -0,0 +1,30 @@
+// Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+// Creare un array di oggetti di studenti.
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+// Dare la possibilità all’utente, attraverso 3 prompt(),
+// di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+var studente = [
+  {
+    nome: 'Walter',
+    cognome: 'Rubicondo',
+    eta: '28',
+  },
+
+  {
+    nome: 'Federico',
+    cognome: 'Figio',
+    eta: '30',
+  },
+
+  {
+    nome: 'Leonardo',
+    cognome: 'Bongianni',
+    eta: '30',
+  }
+];
+
+console.log(studente);
+
+// var lista = ['Walter', 'Federico', 'Alessandro'];
