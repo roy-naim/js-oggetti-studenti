7d85ddf7685b5d14d188a7c8885bb997b9edd178
Roy <b.roynaim@gmail.com>
2021-03-16 15:38:00 +0100

    liste create e aggiunta di nome cognome eta

--- main.js
+++ main.js
@@ -4,27 +4,48 @@
 // Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 // Dare la possibilità all’utente, attraverso 3 prompt(),
 // di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+$(document).ready(function(){
+  var studenti = [
+    {
+      nome: 'Walter',
+      cognome: 'Rubicondo',
+      eta: '28',
+    },
 
-var studente = [
-  {
-    nome: 'Walter',
-    cognome: 'Rubicondo',
-    eta: '28',
-  },
+    {
+      nome: 'Federico',
+      cognome: 'Figio',
+      eta: '30',
+    },
 
-  {
-    nome: 'Federico',
-    cognome: 'Figio',
-    eta: '30',
-  },
+    {
+      nome: 'Leonardo',
+      cognome: 'Bongianni',
+      eta: '30',
+    }
+  ];
 
-  {
-    nome: 'Leonardo',
-    cognome: 'Bongianni',
-    eta: '30',
+  // console.log(studenti);
+
+  for (var i = 0; i < studenti.length; i++) {
+    console.log(studenti[i].nome + ' ' + studenti[i].cognome + ' ' + studenti[i].eta);
   }
-];
 
-console.log(studente);
+  $('#add').click(function () {
+    //$("input[name='nome']")
 
-// var lista = ['Walter', 'Federico', 'Alessandro'];
+    var nome = $("#nome").val();
+    var cognome = $("#cognome").val();
+    var eta = $("#eta").val();
+    var studente = {
+      nome: nome,
+      cognome: cognome,
+      eta: eta,
+    }
+    studenti.push(studente);
+  });
+
+  console.log(studenti);
+
+  // var lista = ['Walter', 'Federico', 'Leonardo'];
+})
